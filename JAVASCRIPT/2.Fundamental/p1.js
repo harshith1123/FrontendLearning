d9344ae5918999196d984ec718fd79977a3d4586
harshith1123 <somethingincredible123@gmail.com>
2025-04-03 09:51:04 +0530

    learned with some basic fundamentals of javascript

---
+++ JAVASCRIPT/2.Fundamental/p1.js
@@ -0,0 +1,56 @@
+// to print any content on DOM
+document.writeln("Welcome to Javascript")
+
+document.writeln("<h1>Welcome to Javascript</h1>")
+document.writeln('<h2>Javascript is a frontend script/h2>')
+
+document.writeln(`<h3>Javascript is an object oriented script</h3>`)
+
+// display image
+document.writeln('<img src="https://png.pngtree.com/png-clipart/20220302/original/pngtree-carnival-mask-png-image_7362400.png" alt="no image"')
+
+// 4 colors in list
+document.writeln("<ol><li>Green</li> <li>Red</li> <li>Pink</li> <li>Orange</li></ol>")
+
+document.writeln(`<ol>
+                    <li>red</li>
+                    <li>green</li>
+                    <li>blue</li>
+                    <li>pink</li>
+                </ol>`)
+ 
+//  print your name (h1), email(h3), mobile number (p) write only one document.write method
+document.writeln(`<div>
+                    <h1>Harshit</h1>
+                    <h2>harshitshetty65@gmail.com</h2>
+                    <p>7019333451</p>
+                </div>`)
+
+// print tables
+
+document.writeln(`<table class="table">
+                    <tr>
+                        <th>Rank</th>
+                        <th>Name</th>
+                        <th>Points</th>
+                        <th>Team</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Domenic</td>
+                        <td>88,110</td>
+                        <td>dcode</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Solly</td>
+                        <td>72,400</td>
+                        <td>Students</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Nick</td>
+                        <td>52,300</td>
+                        <td>dcode</td>
+                    </tr>
+    </table>`)
